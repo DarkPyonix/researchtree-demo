e406412f27113ca60c2ebd08ad5313faf1ebb79e
BREW <81044362+b-re-w@users.noreply.github.com>
2025-04-15 10:25:00 +0900

    Chore: Run MFA with the extended lexicon in the feature extraction instructions

--- scripts/extract_features.py
+++ scripts/extract_features.py
@@ -2,7 +2,8 @@
 
 Durations come from Montreal Forced Aligner TextGrids (data/alignments/<clip>.TextGrid), made with:
 
-    mfa align data/LJSpeech-1.1/wavs data/cmudict-0.7b english_us_arpa data/alignments
+    python scripts/prepare_mfa_lexicon.py > data/lexicon.dict
+    mfa align data/LJSpeech-1.1/wavs data/lexicon.dict english_us_arpa data/alignments
 
 Clips that MFA could not align are skipped and left out of training.
 """
